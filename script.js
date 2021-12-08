6fb291e596e241e4f915ecfe9030bfddcb6bf393
Ken Yamada <yamakenth@gmail.com>
2021-12-08 16:21:08 +0700

    Add function to create and push new Book object into library array

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+// array to store book objects 
+let myLibrary = [];
+
+// book constructor
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-
   this.info = function() {
     return `${title} by ${author}, ${pages} pages, ${(read) ? 'already read' : 'not read yet'}`;
   }
-
 }
 
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
+// example Book object 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 console.log(theHobbit.info());
