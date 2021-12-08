edec224801755e9a6be4462935071905673e5d30
Ken Yamada <yamakenth@gmail.com>
2021-12-08 15:58:14 +0700

    Add Book example from previous lesson

---
+++ script.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = function() {
+    return `${title} by ${author}, ${pages} pages, ${(read) ? 'already read' : 'not read yet'}`;
+  }
+
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+console.log(theHobbit.info());
